af51b07281faa38786e582bafb0acbdc269f3d57
josipas <josipa.supe@endava.com>
2022-07-06 17:29:47 +0200

    LaunchScreen storyboard removed. Set up initial view controller.

--- QuizApp/QuizApp/InitialViewController.swift
+++ QuizApp/QuizApp/InitialViewController.swift
@@ -1,9 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController {
+class InitialViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
     }
+
 }
