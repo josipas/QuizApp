ca50a799ec7c0cea199e20e7251bd2c886e6f781
josipas <josipa.supe@endava.com>
2022-07-07 15:56:42 +0200

    Required changes.

--- QuizApp/QuizApp/CustomInputFieldView.swift
+++ QuizApp/QuizApp/CustomInputFieldView.swift
@@ -1,11 +1,11 @@
-import SnapKit
 import UIKit
+import SnapKit
 
 class CustomInputFieldView: UIView {
 
-    var inputTextField: UITextField!
-    var showPasswordButton: UIButton!
-    var type: CustomInputFieldType?
+    private var inputTextField: UITextField!
+    private var showPasswordButton: UIButton!
+    private var type: CustomInputFieldType!
 
     init(type: CustomInputFieldType) {
         super.init(frame: .zero)
@@ -25,7 +25,7 @@ class CustomInputFieldView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        self.layer.cornerRadius = self.bounds.height / 2
+        layer.cornerRadius = bounds.height / 2
     }
 
 }
@@ -34,18 +34,13 @@ extension CustomInputFieldView: ConstructViewsProtocol {
 
     func createViews() {
         inputTextField = UITextField()
-        showPasswordButton = UIButton()
+        addSubview(inputTextField)
 
-        self.addSubview(inputTextField)
-        self.addSubview(showPasswordButton)
+        showPasswordButton = UIButton()
+        addSubview(showPasswordButton)
     }
 
     func styleViews() {
-        guard
-            let type = type,
-            let placeholder = type.description
-        else { return }
-
         backgroundColor = .white.withAlphaComponent(0.3)
 
         showPasswordButton.isHidden = true
@@ -58,7 +53,7 @@ extension CustomInputFieldView: ConstructViewsProtocol {
         inputTextField.tintColor = .white
         inputTextField.font = .systemFont(ofSize: 16, weight: .bold)
         inputTextField.attributedPlaceholder = NSAttributedString(
-            string: placeholder,
+            string: type.description,
             attributes: [
                 NSAttributedString.Key.foregroundColor: UIColor.white.withAlphaComponent(0.6),
                 NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)
@@ -71,6 +66,10 @@ extension CustomInputFieldView: ConstructViewsProtocol {
     }
 
     func defineLayoutForViews() {
+        snp.makeConstraints {
+            $0.height.equalTo(45)
+        }
+
         inputTextField.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(20)
             $0.trailing.equalTo(showPasswordButton.snp.leading).offset(-20)
@@ -112,12 +111,12 @@ extension CustomInputFieldView: ConstructViewsProtocol {
 extension CustomInputFieldView: UITextFieldDelegate {
 
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        self.layer.borderWidth = 1
-        self.layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.white.cgColor
     }
 
     func textFieldDidEndEditing(_ textField: UITextField) {
-        self.layer.borderWidth = 0
+        layer.borderWidth = 0
 
         if type == .password {
             showPasswordButton.isHidden = true
